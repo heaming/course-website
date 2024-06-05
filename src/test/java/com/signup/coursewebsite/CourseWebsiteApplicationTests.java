6e0d02b2479601b01f53ad95d806b2cd8718df6f
heaming <meetoohi@gmail.com>
2024-06-05 17:33:45 +0900

    Initial commit

---
+++ src/test/java/com/signup/coursewebsite/CourseWebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.signup.coursewebsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseWebsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
